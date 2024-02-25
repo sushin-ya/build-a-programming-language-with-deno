7b4b7faaa8b9ba2a10c7e67e1230c815fa538c4d
tsukaryu <drgon8otsuka@gmail.com>
2024-02-25 10:56:58 +0900

    add readme

--- lexer.ts
+++ lexer.ts
@@ -1,2 +1,46 @@
-// Let x = 45
+// Let x = 45 + {foo * bar}
 // [LetToken, IdentifierTk, EqualLsToken , NumberToken]
+
+export enum TokenType {
+  Number,
+  Identifier,
+  Equals,
+  OpenParen,
+  CloseParen,
+  BinaryOperator,
+  Let,
+}
+
+export interface Token {
+  value: string;
+  type: TokenType;
+}
+
+function token(value = "", type: TokenType): Token {
+  return { value, type };
+}
+
+export function tokenize(sourceCode: string): Token[] {
+  const tokens = new Array<Token>();
+  const src = sourceCode.split("");
+
+  // Build each token until end of file
+  while (src.length > 0) {
+    if (src[0] == "(") {
+      tokens.push(token(src.shift(), TokenType.OpenParen));
+    } else if (src[0] == ")") {
+      tokens.push(token(src.shift(), TokenType.CloseParen));
+    } else if (
+      src[0] == "+" ||
+      src[0] == "-" ||
+      src[0] == "*" ||
+      src[0] == "/"
+    ) {
+      tokens.push(token(src.shift(), TokenType.BinaryOperator));
+    } else if (src[0] == "=") {
+      tokens.push(token(src.shift(), TokenType.Equals));
+    }
+  }
+
+  return tokens;
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+### How To Build A Programming Language From Scratch
+https://www.youtube.com/watch?v=8VB5TY1sIRo&list=PL_2VhOvlMk4UHGqYCLWc6GO8FaPl8fQTh
