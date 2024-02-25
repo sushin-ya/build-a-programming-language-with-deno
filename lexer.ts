d8313a31ff14b952f8b660f9ce311d50b8f1908b
tsukaryu <drgon8otsuka@gmail.com>
2024-02-25 11:19:06 +0900

    part1: tokenizerを実装

--- lexer.ts
+++ lexer.ts
@@ -11,6 +11,10 @@ export enum TokenType {
   Let,
 }
 
+const KEYWORDS: Record<string, TokenType> = {
+  let: TokenType.Let,
+};
+
 export interface Token {
   value: string;
   type: TokenType;
@@ -18,6 +22,20 @@ export interface Token {
 
 function token(value = "", type: TokenType): Token {
   return { value, type };
+}
+
+function isalpha(src: string): boolean {
+  return src.toUpperCase() != src.toLowerCase();
+}
+
+function isslippable(str: string): boolean {
+  return str == " " || str == "\n" || str == "\t";
+}
+
+function isint(str: string): boolean {
+  const c = str.charCodeAt(0);
+  const bounds = ["0".charCodeAt(0), "9".charCodeAt(0)];
+  return c >= bounds[0] && c <= bounds[1];
 }
 
 export function tokenize(sourceCode: string): Token[] {
@@ -39,6 +57,36 @@ export function tokenize(sourceCode: string): Token[] {
       tokens.push(token(src.shift(), TokenType.BinaryOperator));
     } else if (src[0] == "=") {
       tokens.push(token(src.shift(), TokenType.Equals));
+    } else {
+      // Handle multi character tokens
+
+      // Build number token
+      if (isint(src[0])) {
+        let num = "";
+        while (src.length > 0 && isint(src[0])) {
+          num += src.shift();
+        }
+
+        tokens.push(token(num, TokenType.Number));
+      } else if (isalpha(src[0])) {
+        let ident = ""; // 変数fooなのか、変数宣言のLetなのかわからない
+        while (src.length > 0 && isalpha(src[0])) {
+          ident += src.shift();
+        }
+
+        // check for reserved keywords
+        const reserved = KEYWORDS[ident];
+        if (reserved == undefined) {
+          tokens.push(token(ident, TokenType.Identifier));
+        } else {
+          tokens.push(token(ident, reserved));
+        }
+      } else if (isslippable(src[0])) {
+        src.shift(); // SKIP THE CURRENT CHARACTER
+      } else {
+        console.log("Unrecognized character found in source: ", src[0]);
+        Deno.exit(1);
+      }
     }
   }
 
