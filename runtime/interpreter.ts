8b110f34618a86db7b457f599080b4396390a00b
tsukaryu <drgon8otsuka@gmail.com>
2024-02-28 23:44:46 +0900

    part4: 二項演算子の評価器

--- runtime/interpreter.ts
+++ runtime/interpreter.ts
@@ -1,5 +1,55 @@
-import { NumberVal, NullVal, RuntimeVal, ValueType } from "./values.ts";
-import { NodeType, NumericLiteral, Stmt } from "./../frontend/ast.ts";
+import { NumberVal, NullVal, RuntimeVal } from "./values.ts";
+import {
+  BinaryExpr,
+  NumericLiteral,
+  Program,
+  Stmt,
+} from "./../frontend/ast.ts";
+
+function eval_program(program: Program): RuntimeVal {
+  let lastEvaluated: RuntimeVal = {
+    type: "null",
+    value: "null",
+  } as NullVal;
+
+  for (const statement of program.body) {
+    lastEvaluated = evaluate(statement);
+  }
+
+  return lastEvaluated;
+}
+
+function eval_numeric_binary_expr(
+  lhs: NumberVal,
+  rhs: NumberVal,
+  operator: string
+): NumberVal {
+  let result: number;
+  if (operator == "+") result = lhs.value + rhs.value;
+  else if (operator == "-") result = lhs.value - rhs.value;
+  else if (operator == "*") result = lhs.value * rhs.value;
+  else if (operator == "/")
+    result = lhs.value / rhs.value; // TODO: Division by zero checks;
+  else result = lhs.value % rhs.value;
+
+  return { value: result, type: "number" };
+}
+
+function evaluate_binary_expr(binop: BinaryExpr): RuntimeVal {
+  const lhs = evaluate(binop.left);
+  const rhs = evaluate(binop.right);
+
+  if (lhs.type == "number" && rhs.type == "number") {
+    return eval_numeric_binary_expr(
+      lhs as NumberVal,
+      rhs as NumberVal,
+      binop.operator
+    );
+  }
+
+  // One or both are NULL
+  return { type: "null", value: "null" } as NullVal;
+}
 
 export function evaluate(astNode: Stmt): RuntimeVal {
   switch (astNode.kind) {
@@ -8,8 +58,16 @@ export function evaluate(astNode: Stmt): RuntimeVal {
         type: "number",
         value: (astNode as NumericLiteral).value,
       } as NumberVal;
+
     case "NullLiteral":
       return { value: "null", type: "null" } as NullVal;
+
+    case "BinaryExpr":
+      return evaluate_binary_expr(astNode as BinaryExpr);
+
+    case "Program":
+      return eval_program(astNode as Program);
+
     default:
       console.log(
         "This AST Node has not yet been setup for interpretation",
