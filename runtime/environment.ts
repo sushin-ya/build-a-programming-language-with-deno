3fbaf55141d538fe4feba94327c9d63331aa2ac2
tsukaryu <drgon8otsuka@gmail.com>
2024-03-02 11:06:37 +0900

    part6: error handle

--- runtime/environment.ts
+++ runtime/environment.ts
@@ -3,23 +3,38 @@ import { RuntimeVal } from "./values.ts";
 export default class Environment {
   private parent?: Environment;
   private variables: Map<string, RuntimeVal>;
+  private constants: Set<string>;
 
   constructor(parentENV?: Environment) {
     this.parent = parentENV;
     this.variables = new Map();
+    this.constants = new Set();
   }
 
-  public declareVar(varname: string, value: RuntimeVal): RuntimeVal {
+  public declareVar(
+    varname: string,
+    value: RuntimeVal,
+    constant: boolean
+  ): RuntimeVal {
     if (this.variables.has(varname)) {
       throw `Cannot declare variable ${varname}. As it already is defined.`;
     }
 
     this.variables.set(varname, value);
+
+    if (constant) {
+      this.constants.add(varname);
+    }
     return value;
   }
 
   public assignVar(varname: string, value: RuntimeVal): RuntimeVal {
     const env = this.resolve(varname);
+
+    // Cannot assign to constant
+    if (env.constants.has(varname)) {
+      throw `Cannot reassign to variable ${varname} as it was declared constants.`;
+    }
     env.variables.set(varname, value);
     return value;
   }
