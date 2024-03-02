f2fbdc8e58850c6f015dbfd7346be0c097c08f73
tsukaryu <drgon8otsuka@gmail.com>
2024-03-02 11:33:02 +0900

    part7: fix error

--- main.ts
+++ main.ts
@@ -8,10 +8,10 @@ repl();
 function repl() {
   const parser = new Parser();
   const env = new Environment();
-  env.declareVar("x", MK_NUMBER(100));
-  env.declareVar("true", MK_BOOL(true));
-  env.declareVar("false", MK_BOOL(false));
-  env.declareVar("null", MK_NULL());
+  env.declareVar("true", MK_BOOL(true), true);
+  env.declareVar("false", MK_BOOL(false), true);
+  env.declareVar("null", MK_NULL(), true);
+
   console.log("\nRepl v0.1");
   while (true) {
     const input = prompt("> ");
