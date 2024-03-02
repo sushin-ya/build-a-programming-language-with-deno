99085e93f2dfeab15820b77164199ea377abcbf5
tsukaryu <drgon8otsuka@gmail.com>
2024-03-02 10:54:43 +0900

    part6: interpreterをrefactor

---
+++ runtime/eval/expressions.ts
@@ -0,0 +1,47 @@
+import { BinaryExpr, Identifier } from "../../frontend/ast.ts";
+import Environment from "../environment.ts";
+import { evaluate } from "../interpreter.ts";
+import { MK_NULL, NumberVal, RuntimeVal } from "../values.ts";
+
+function eval_numeric_binary_expr(
+  lhs: NumberVal,
+  rhs: NumberVal,
+  operator: string
+): NumberVal {
+  let result: number;
+  if (operator == "+") result = lhs.value + rhs.value;
+  else if (operator == "-") result = lhs.value - rhs.value;
+  else if (operator == "*") result = lhs.value * rhs.value;
+  else if (operator == "/")
+    result = lhs.value / rhs.value; // TODO: Division by zero checks;
+  else result = lhs.value % rhs.value;
+
+  return { value: result, type: "number" };
+}
+
+export function evaluate_binary_expr(
+  binop: BinaryExpr,
+  env: Environment
+): RuntimeVal {
+  const lhs = evaluate(binop.left, env);
+  const rhs = evaluate(binop.right, env);
+
+  if (lhs.type == "number" && rhs.type == "number") {
+    return eval_numeric_binary_expr(
+      lhs as NumberVal,
+      rhs as NumberVal,
+      binop.operator
+    );
+  }
+
+  // One or both are NULL
+  return MK_NULL();
+}
+
+export function eval_identifier(
+  ident: Identifier,
+  env: Environment
+): RuntimeVal {
+  const val = env.lookupVar(ident.symbol);
+  return val;
+}
--- runtime/interpreter.ts
+++ runtime/interpreter.ts
@@ -5,56 +5,11 @@ import {
   Program,
   Stmt,
   Identifier,
+  VarDeclaration,
 } from "./../frontend/ast.ts";
 import Environment from "./environment.ts";
-import { MK_NULL } from "./values.ts";
-
-function eval_program(program: Program, env: Environment): RuntimeVal {
-  let lastEvaluated: RuntimeVal = MK_NULL();
-
-  for (const statement of program.body) {
-    lastEvaluated = evaluate(statement, env);
-  }
-
-  return lastEvaluated;
-}
-
-function eval_numeric_binary_expr(
-  lhs: NumberVal,
-  rhs: NumberVal,
-  operator: string
-): NumberVal {
-  let result: number;
-  if (operator == "+") result = lhs.value + rhs.value;
-  else if (operator == "-") result = lhs.value - rhs.value;
-  else if (operator == "*") result = lhs.value * rhs.value;
-  else if (operator == "/")
-    result = lhs.value / rhs.value; // TODO: Division by zero checks;
-  else result = lhs.value % rhs.value;
-
-  return { value: result, type: "number" };
-}
-
-function evaluate_binary_expr(binop: BinaryExpr, env: Environment): RuntimeVal {
-  const lhs = evaluate(binop.left, env);
-  const rhs = evaluate(binop.right, env);
-
-  if (lhs.type == "number" && rhs.type == "number") {
-    return eval_numeric_binary_expr(
-      lhs as NumberVal,
-      rhs as NumberVal,
-      binop.operator
-    );
-  }
-
-  // One or both are NULL
-  return MK_NULL();
-}
-
-function eval_identifier(ident: Identifier, env: Environment): RuntimeVal {
-  const val = env.lookupVar(ident.symbol);
-  return val;
-}
+import { eval_identifier, evaluate_binary_expr } from "./eval/expressions.ts";
+import { eval_program, eval_var_declaration } from "./eval/statements.ts";
 
 export function evaluate(astNode: Stmt, env: Environment): RuntimeVal {
   switch (astNode.kind) {
@@ -72,6 +27,9 @@ export function evaluate(astNode: Stmt, env: Environment): RuntimeVal {
 
     case "Program":
       return eval_program(astNode as Program, env);
+
+    case "VarDeclaration":
+      return eval_var_declaration(astNode as VarDeclaration, env);
 
     default:
       console.log(
